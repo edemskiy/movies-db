cd2d202b80f99c468894a6f9d79e8c2a16f04b13
edemskiy <qapple@ya.ru>
2020-02-07 19:23:32 +0300

    Add movie details to sidebar

--- src/pages/MoviePage/Sidebar/Styles.js
+++ src/pages/MoviePage/Sidebar/Styles.js
@@ -5,8 +5,20 @@ export const SidebarWrapper = styled.div`
   width: 30vw;
   height: auto;
   padding: 10px;
-  background-color: grey;
+  background-color: #efefef;
   @media ${device.sm} {
     width: 100%;
   }
 `;
+
+export const MovieInfo = styled.div`
+  margin-left: 15px;
+`;
+
+export const MovieProperty = styled.div`
+  margin-top: 10px;
+`;
+export const PropertyTitle = styled.p`
+  font-weight: 700;
+`;
+export const PropertyValue = styled.p``;
--- src/pages/MoviePage/Sidebar/index.jsx
+++ src/pages/MoviePage/Sidebar/index.jsx
@@ -1,6 +1,51 @@
 import React from "react";
-import { SidebarWrapper } from "./Styles";
+import {
+  SidebarWrapper,
+  MovieInfo,
+  MovieProperty,
+  PropertyTitle,
+  PropertyValue
+} from "./Styles";
 
+function getRuntimeString(minutes) {
+  return `${(minutes - (minutes % 60)) / 60}h ${minutes % 60}m`;
+}
+
+function getLocaleDate(dateString) {
+  return new Date(dateString).toLocaleString(undefined, {
+    day: "2-digit",
+    month: "long",
+    year: "numeric"
+  });
+}
+
+function getSidebarMovieProps(movie) {
+  return Object.keys(movie).length
+    ? {
+        Budget: `$${movie["budget"]}`,
+        Genres: movie["genres"].map(genre => genre.name).join(", "),
+        Country: movie["production_countries"]
+          .map(country => country["iso_3166_1"])
+          .join(", "),
+        "Release date": getLocaleDate(movie["release_date"]),
+        "Box office": `$${movie["revenue"]}`,
+        Runtime: getRuntimeString(movie["runtime"])
+      }
+    : null;
+}
 export function Sidebar({ movie }) {
-  return <SidebarWrapper></SidebarWrapper>;
+  const movieProps = getSidebarMovieProps(movie);
+  return (
+    <SidebarWrapper>
+      <MovieInfo>
+        {movieProps &&
+          Object.keys(movieProps).map(propName => (
+            <MovieProperty key={propName}>
+              <PropertyTitle>{propName}</PropertyTitle>
+              <PropertyValue>{movieProps[propName] || "No info"}</PropertyValue>
+            </MovieProperty>
+          ))}
+      </MovieInfo>
+    </SidebarWrapper>
+  );
 }
